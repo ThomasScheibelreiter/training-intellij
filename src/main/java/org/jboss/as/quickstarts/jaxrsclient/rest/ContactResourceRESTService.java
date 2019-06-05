aefeba9885497fb5fc991e32f5cfe057f0f19d24
Thomas Scheibelreiter <thomas.scheibelreiter@noegkk.at>
2019-06-05 12:44:18 +0200

    refactor

--- src/main/java/org/jboss/as/quickstarts/jaxrsclient/rest/ContactResourceRESTService.java
+++ src/main/java/org/jboss/as/quickstarts/jaxrsclient/rest/ContactResourceRESTService.java
@@ -16,82 +16,77 @@
  */
 package org.jboss.as.quickstarts.jaxrsclient.rest;
 
+
+import org.jboss.as.quickstarts.jaxrsclient.model.Contact;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.jboss.as.quickstarts.jaxrsclient.model.Contact;
 
 @Path("/contacts")
 public class ContactResourceRESTService {
 
-    private static Map<Long, Contact> contactsRepository = new HashMap<>();
+	private static Map<Long, Contact> contactsRepository = new HashMap<>();
 
-    /**
-     * Creates a new contact from the values provided and will return a JAX-RS response with either 200 ok, or 400 (BAD REQUEST)
-     * in case of errors.
-     */
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response createContact(Contact contact) {
+	/**
+	 * Creates a new contact from the values provided and will return a JAX-RS response with either 200 ok, or 400 (BAD REQUEST)
+	 * in case of errors.
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createContact(Contact contact) {
 
-        Response.ResponseBuilder builder = null;
-        Long nextId = contactsRepository.keySet().size() + 1L;
-        try {
-            // Store the contact
-            contact.setId(nextId);
-            contactsRepository.put(nextId, contact);
+		Long nextId = contactsRepository.keySet().size() + 1L;
+		try {
+			if (contact.getName() == null) {
+				return Response.status(Response.Status.BAD_REQUEST).build();
+			}
+			// Store the contact
+			contact.setId(nextId);
+			contactsRepository.put(nextId, contact);
 
-            // Create an "ok" response with the persisted contact
-            builder = Response.ok(contact);
-        } catch (Exception e) {
-            // Handle generic exceptions
-            builder = Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage());
-        }
+			// Create an "ok" response with the persisted contact
+			return Response.ok(contact).build();
+		} catch (Exception e) {
+			// Handle generic exceptions
+			return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+		}
+	}
 
-        return builder.build();
-    }
+	// delete all contacts
+	@DELETE
+	public Response removeAllContacts() {
+		contactsRepository.clear();
+		return Response.ok().build();
+	}
 
-    // delete all contacts
-    @DELETE
-    public Response removeAllContacts() {
-        contactsRepository.clear();
-        return Response.ok().build();
-    }
+	// delete a specific contact
+	@DELETE
+	@Path("/{id}")
+	public Response removeContact(final @PathParam("id") Long id) {
+		contactsRepository.remove(id);
+		return Response.ok().build();
+	}
 
-    // delete a specific contact
-    @DELETE
-    @Path("/{id}")
-    public Response removeContact(final @PathParam("id") Long id) {
-        contactsRepository.remove(id);
-        return Response.ok().build();
-    }
+	// Fetch all contacts
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAll() {
+		Collection<Contact> allcontacts = contactsRepository.values();
+		return Response.ok(allcontacts).build();
+	}
 
-    // Fetch all contacts
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getAll() {
-        Collection<Contact> allcontacts = contactsRepository.values();
-        return Response.ok(allcontacts).build();
-    }
-
-    // Fetch a specific contact
-    @GET
-    @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getById(final @PathParam("id") Long id) {
-        Contact contact = contactsRepository.get(id);
-        return Response.ok(contact).build();
-    }
+	// Fetch a specific contact
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getById(final @PathParam("id") Long id) {
+		Contact contact = contactsRepository.get(id);
+		return Response.ok(contact).build();
+	}
 }
