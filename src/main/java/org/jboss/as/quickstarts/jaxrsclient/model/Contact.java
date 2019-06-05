d2910e36cf97317300908a362e517adc6e2e3913
Thomas Scheibelreiter <thomas.scheibelreiter@noegkk.at>
2019-06-05 10:46:19 +0200

    test

--- src/main/java/org/jboss/as/quickstarts/jaxrsclient/model/Contact.java
+++ src/main/java/org/jboss/as/quickstarts/jaxrsclient/model/Contact.java
@@ -16,46 +16,47 @@
  */
 package org.jboss.as.quickstarts.jaxrsclient.model;
 
+
 public class Contact {
 
-    private Long id;
+	private Long id;
 
-    private String name;
+	private String name;
 
-    private String phoneNumber;
+	private String phoneNumber;
 
-    private String savedBy;
+	private String savedBy;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
-    public String getSavedBy() {
-        return savedBy;
-    }
+	public String getSavedBy() {
+		return savedBy;
+	}
 
-    public void setSavedBy(String savedBy) {
-        this.savedBy = savedBy;
-    }
+	public void setSavedBy(String savedBy) {
+		this.savedBy = savedBy;
+	}
 
 }
